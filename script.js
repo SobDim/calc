a0c3e933ec14846f59cf48fb6cffdefee7219e27
SobDim <101928077+SobDim@users.noreply.github.com>
2024-02-20 13:26:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let a = '';
+let b = '';
+let result = document.querySelector('result');
+let operation = '';
+document.querySelector('.buttons');
+
+document.querySelector('.buttuns').addEventListener('click', event => {
+  result.innerText = event.target.innerText;
+  console.log(result);
+});
+
+5;
+console.log({});
+
+console.log([]);
+
+console.log(x => x * 2);
